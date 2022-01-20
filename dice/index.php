5161bbd75d5d08f1ea7ee3fabffc3ae62378b5f7
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2022-01-20 14:52:41 +0900

    Add files via upload

--- dice/index.php
+++ dice/index.php
@@ -1,15 +1,29 @@
 <?php
 
+$dice1 = mt_rand(1,6);
+$dice2 = mt_rand(1,6);
+$zorome = ($dice1 == $dice2) ? true : false;
+
 ?>
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
   <head>
     <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
+    <title>サイコロ</title>
   </head>
   <body>
-    
+    <h1>サイコロ</h1>
+    <p>サイコロの目は「<?php echo $dice1; ?>」「<?php echo $dice2; ?>」でした！
+    <?php if($zorome) : ?>
+      ゾロ目です！
+    <?php endif; ?>
+  </p>
+
+  <P?<?php    ?>回目でぞろ目になりました</p>
+    <p><a href="<?php echo $_SERVER["SCRIPT_NAME"]; ?>">もう一度！</a>
+
   </body>
 </html>
